e515a8991309bf4e5cd87ea079e7623ab6fb96d1
birukabel <biruk.abel@yahoo.com>
2023-02-11 20:29:31 +0300

    initla commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string s = "45678";
+char[] ch = s.ToCharArray();
+int sum = 0;
+foreach(char c in ch)
+{
+    //Console.WriteLine(c);
+    //Console.WriteLine(Convert.ToInt32(c.ToString()));
+    sum += Convert.ToInt32(c.ToString());
+}
+Console.WriteLine(sum);
+
+int num=2456;
+char[] chNums = num.ToString().ToCharArray();
+sum = 0;
+foreach(char cr in chNums)
+{
+    sum += Convert.ToInt32(cr.ToString());
+}
+Console.WriteLine(sum);
